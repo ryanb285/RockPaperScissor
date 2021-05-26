abefc2e2f8c16b88b04678a3d32d0dbb8f96581b
ryanb285 <73402247+ryanb285@users.noreply.github.com>
2021-05-25 20:48:51 -0400

    Update RPS.py
    8:48 pm, imports random

--- RPS.py
+++ RPS.py
@@ -1,5 +1,7 @@
 # RPS.py
 
+import random
+
 print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
@@ -8,12 +10,15 @@ print(user_choice)
 
 if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors")
     print("Good choice!")
-else
-    print("Game Canceled")
+else:
+    print("Game canceled")
     exit()
 
+computer_choice = random.choice("rock", "paper", "scissor")
 
 
 
 
-#Print("THSI IS THE END OF OR GAME. PLEASE PLAY AGAIN")
+
+
+print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN")
