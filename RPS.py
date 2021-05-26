0ef66dd767115552d1d322f47489c6da735ebe70
ryanb285 <73402247+ryanb285@users.noreply.github.com>
2021-05-25 20:34:05 -0400

    Update RPS.py
    8:33pm EST

--- RPS.py
+++ RPS.py
@@ -4,6 +4,16 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
-print("USER CHOICE: ", user_choice)
+print(user_choice)
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors")
+    print("Good choice!")
+else
+    print("Game Canceled")
+    exit()
 
 
+
+
+
+#Print("THSI IS THE END OF OR GAME. PLEASE PLAY AGAIN")
