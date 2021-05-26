7c2e05de0b6bd9f7ff955bebcc169bf12393b0ea
ryanb285 <73402247+ryanb285@users.noreply.github.com>
2021-05-25 21:09:27 -0400

    working .py file
    9:09pm

--- RPS.py
+++ RPS.py
@@ -8,14 +8,14 @@ user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 print(user_choice)
 
-if(user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors")
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
     print("Good choice!")
 else:
     print("Game canceled")
     exit()
 
 
-valid_options = ["rock", "paper", "Scissors"]
+valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("Computer choice: ", computer_choice)
 
