969c2a43d509f7aa9a812b28e3d993a74eff9c63
ryanb285 <73402247+ryanb285@users.noreply.github.com>
2021-05-27 18:35:19 -0400

    Adding the conditions for a W

--- RPS.py
+++ RPS.py
@@ -12,16 +12,50 @@ if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "sciss
     print("Good choice!")
 else:
     print("Game canceled")
-    exit()
+
 
 
 valid_options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(valid_options)
 print("Computer choice: ", computer_choice)
 
+#this is probably the least useful way possible but hey! it works. 
+
+if(user_choice == "rock") and (computer_choice == "rock") :
+    print("tie")
+
+if(user_choice == "rock") and (computer_choice == "paper"):
+    print("Computer wins!")
+
+if(user_choice == "rock") and (computer_choice == "scissors"):
+    print("User Wins!")
+
+if(user_choice == "paper") and (computer_choice == "rock"):
+    print("User Wins!")
 
 
+if(user_choice == "paper") and (computer_choice == "paper"):
+    print("tie")
+
+if(user_choice == "paper") and (computer_choice == "scissors"):
+    print("Computer wins!")
+
+if(user_choice == "scissors") and (computer_choice == "rock"):
+    print("Computer wins!")
 
 
+if(user_choice == "scissors") and (computer_choice == "scissors"):
+    print("tie")
+
+if(user_choice == "scissors") and (computer_choice == "paper"):
+    print("User Wins!")
 
 print("THIS IS THE END OF OUR GAME. PLEASE PLAY AGAIN")
+
+exit()
+
+    
+    
+
+
+
